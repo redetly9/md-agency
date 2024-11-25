2554605845cee1a3f72840f06a466f0127d81d8e
Almishev Nurbol <redetly9@gmail.com>
2024-11-26 02:02:14 +0500

    xcxccx

---
+++ lib/edgestore.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+// import { type EdgeStoreRouter } from "@/app/api/edgestore/[...edgestore]/route";
+// import { createEdgeStoreProvider } from "@edgestore/react";
+
+// const { EdgeStoreProvider, useEdgeStore } =
+//   createEdgeStoreProvider<EdgeStoreRouter>();
+
+const EdgeStoreProvider = ({ children, basePath }: any): any => {
+  return <></>;
+};
+
+function useEdgeStore(): any {
+  return {};
+}
+
+export { EdgeStoreProvider, useEdgeStore };
